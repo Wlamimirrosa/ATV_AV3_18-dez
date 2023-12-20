9dacb4f584b1799679284d79fba43367c0dbbe13
Wlamimirrosa <129913640+Wlamimirrosa@users.noreply.github.com>
2023-12-20 14:53:54 -0300

    Produto.php

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'nome', 'preco', 'categoria_id',
+    ];
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+
+    public function pedidos()
+    {
+        return $this->hasMany(Pedido::class);
+    }
 }
