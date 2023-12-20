696298999d2b2f7a2bc1dd81ac9346976f72941e
Wlamimirrosa <129913640+Wlamimirrosa@users.noreply.github.com>
2023-12-20 14:53:36 -0300

    Pedido.php

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'produto_id', 'quantidade', 'valor_total',
+    ];
+
+    public function produto()
+    {
+        return $this->belongsTo(Produto::class);
+    }
 }
